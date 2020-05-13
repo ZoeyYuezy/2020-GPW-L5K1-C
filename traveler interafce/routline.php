7950fb54570239d405c1bef12c319eebbd8a41b4
amyzhang7 <45256847+amyzhang7@users.noreply.github.com>
2020-05-13 23:07:31 +0800

    Update routline.php

--- traveler interafce/routline.php
+++ traveler interafce/routline.php
@@ -1,3 +1,37 @@
+   <?php
+
+// 1. Open database connection
+require ('db.php');
+
+// 2. Do a query
+$query  = "SELECT initial_location, destination, date, price "; 
+$query .= "FROM routline";
+$result = mysqli_query($connection,$query);
+
+if (!$result) {
+    die("query is wrong");
+}
+
+    
+            
+// 3. use/show data
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row["initial_location"] . "</td>";
+    echo "<td>" . $row["destination"] . "</td>";
+    echo "<td>" . $row["date"] . "</td>";
+    echo "<td>" . $row["price"] . "</td>";
+    echo "<td><a href='rd.php?id=" . $row["id"] ."'>Cheak</a></td>";
+    echo "</tr>";
+}
+    
+   
+// 4. free results
+mysqli_free_result($result);
+
+// 5. close db connection
+mysqli_close($connection);
+?>
 <!doctype html>
 <!--[if IE 7]>    <html class="ie7" > <![endif]-->
 <!--[if IE 8]>    <html class="ie8" > <![endif]-->
@@ -8,9 +42,8 @@
 				<!-- META TAGS -->
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width" />
-				
 				<!-- Title -->
-				<title>CBC</title>
+				<title>Bus Information</title>
                 <link href='http://fonts.useso.com/css?family=Open+Sans+Condensed:300,400,600,800,700' rel='stylesheet' type='text/css'>
 
             <!-- Style Sheet-->
@@ -21,6 +54,8 @@
 
                <!-- favicon -->
 				<link rel="shortcut icon" href="images/favicon.png">
+
+<link rel="stylesheet" href="css/matrix-style.css" />
 				
 				<!--[if lt IE 9]>
 						<script src="js/html5shiv.js"></script>
@@ -53,7 +88,7 @@
                                         <a href="#" class="google"></a>
                                         <a href="#" class="rss"></a>
                                     </div>
-                                    <a href="login.php" class="sign-in" id="login-link">Sign in</a>
+                                    <a href="login.php" class="sign-in" id="login-link">Sign Out</a>
                                 </div>
                             </div>
                         </div>
@@ -70,7 +105,7 @@
                         	<!-- Logo -->
                             <div class="span4">
                             	<div class="logo">
-                                	<a href="index.php"><img src="images/logo.png" alt="Logo"></a>
+                                	<a href="index.php"><img src="Images/logo.png" alt="Logo"></a>
                                 </div>
                             </div>
                         	<!-- Logo -->
@@ -105,8 +140,7 @@
                                     <ul>
                                         <li><a href="index.php">Home </a>
                                         	<ul class="clearfix">
-                                                <li><a href="index2.html">Home 2</a></li>
-                                                <li><a href="index3.html">Home 3 </a></li>
+                                                <li><a href="login.php">Home 2</a></li>
                                             </ul>
                                         </li>
                                         
@@ -129,7 +163,7 @@
                                                 <li><a href="#">Projection </a> </li>
                                             </ul>
                                         </li>
-                                        <li><a href="#">Cars</a></li>
+                                        <li><a href="routline.php">Cars</a></li>
                                         <li><a href="#">Vacations</a></li>
                                         <li><a href="#">Guide Book</a></li>
                                         <li><a href="#">Hot Deal</a></li>
@@ -147,7 +181,7 @@
                                         <li class="open">
                                             <a href="index.php">HOME</a>
                                             <ul>
-                                                <li><a href="#">Home </a></li>
+                                                <li><a href="index.php">Home </a></li>
                                                 <li><a href="#">Hotels</a></li>
                                                 <li><a href="#">Holidays</a></li>
                                                 <li><a href="#">Flights</a> </li>
@@ -157,7 +191,7 @@
                                                 <li><a href="#">Television </a> </li>
                                                 <li><a href="#">Smart Phone </a> </li>
                                                 <li><a href="#">Projection </a> </li>
-                                                <li><a href="#">Cars</a></li>
+                                                <li><a href="routline.php">Cars</a></li>
                                                 <li><a href="#">Vacations</a></li>
                                                 <li><a href="#">Guide Book</a></li>
                                                 <li><a href="#">Hot Deal</a></li>
@@ -180,175 +214,72 @@
                     	<div class="row">
                         	<div class="span12">
                             	<a href="index.php">Home</a>
-                                <span class="crum">Booking</span>
+                                <span class="crum">Routline</span>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- Crum-->
-
-                
-                 <?php
-
-// 1. Open database connection
-require ('db.php');
-
-// 2. Do a query
-$query  = "SELECT initial_location, destination, date, price "; 
-$query .= "FROM routline";
-$result = mysqli_query($connection,$query);
-
-if (!$result) {
-    die("query is wrong");
-}
-?>
-               	<div class="specialoffer-wrapper">
-                	<div class="container">
-                    	
-                        <!-- Heading -->
-                        <div class="row">
-                        	<div class="span12">
-                            	<div class="heading">
-                                	<h2>Car<span>Routlines</span></h2>
-                                </div>
-                            </div>
-                        </div>
-                        
-                	</div>
-                </div> 
-            <table width="80%" border="double" cellpadding="2" cellspacing="1" align="center" >
-                <tr>
-     <td>Start Place</td>
-     <td>End Place</td>
-     <td>Date</td> 
-     <td>Price</td> 
-    
-     </tr>
-      
-              
-
             
-            <?php
+                <!-- Grid page -->
+                <div class="content booking_wrap">
+                    <div class="container">
+                        <div class="row">
+                            <div class="span12 booking clearfix">
+                                <div class="top">
+                                    <h2>Routline</h2>
+                                    <h3>Chengdu-Yaan</h3>
+                                    <div class="stars">
+                                        <a href="#" class="active"></a>
+                                        <a href="#" class="active"></a>
+                                        <a href="#" class="active"></a>
+                                        <a href="#"></a>
+                                        <a href="#"></a>
+                                    </div>
+                                </div>
+                                
+            <div class="bottom clearfix">                    
+        <table class="table table-bordered table-striped with-check">
+            <thead>
+                <tr>
+                <th>Start Place</th>
+                <th>End Place</th>
+                 <th>Date</th>
+                <th>Distance</th>
+                <th>Price</th>   
+            </thead>
+
+<?php
+
 // 3. use/show data
 while ($row = mysqli_fetch_array($result)) {
     echo "<tr>";
     echo "<td>" . $row["initial_location"] . "</td>";
     echo "<td>" . $row["destination"] . "</td>";
+     echo "<td>" . $row["date"] . "</td>";
     echo "<td>" . $row["date"] . "</td>";
     echo "<td>" . $row["price"] . "</td>";
-    echo "<td><a href='rd.php?id=" . $row["id"] ."'>Cheak</a></td>";
-    echo "</tr>";
 }
     
 ?>
-
-
     
-
+</table>
+  </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>       
     
-<?php   
+<?php
+
 // 4. free results
 mysqli_free_result($result);
 
 // 5. close db connection
 mysqli_close($connection);
+
 ?>
-
-
-
-
-
-                <!-- Footer widget -->
-                <div class="footer-widget-wrapper">
-                    <div class="container">
-                        <div class="row">
-
-                            <div class="span3 f-widget copy-right">
-                                <a href="#" class="f-logo"><img src="images/footer-logo.png" alt="Logo"></a>
-                                <p>© 2013 <a href="#">CBC</a>. All rights reserved</p>
-                                <p>Designed by Group C</p>
-                            </div>
-                            <div class="span3 f-widget">
-                                <h4>Company Infomation</h4>
-                                <ul>
-                                    <li><a href="#">About US</a></li>
-                                    <li><a href="#">Team</a></li>
-                                    <li><a href="#">Booking Tips</a></li>
-                                    <li><a href="#">Payment Option</a></li>
-                                    <li class="last"><a href="#">Infomation</a></li>
-                                </ul>
-                            </div>
-                            <div class="span3 f-widget">
-                                <h4>Customer Care</h4>
-                                <ul>
-                                    <li><a href="#">About US</a></li>
-                                    <li><a href="#">Team</a></li>
-                                    <li><a href="#">Booking Tips</a></li>
-                                    <li><a href="#">Payment Option</a></li>
-                                    <li class="last"><a href="#">Infomation</a></li>
-                                </ul>
-                            </div>
-                            <div class="span3 f-widget">
-                                <div class="cc">
-                                    <h4>Customer Support</h4>
-                                    <h2>1-669-559-4378</h2>
-                                    <span class="pull-right">Support 24/24</span>
-                                </div>
-                                <div class="f-widget n-letter">
-                                    <h4>Newsletter</h4>
-
-                                    <form>
-                                        <input type="text" name="newlatter" value="Enter your email...">
-                                        <input type="submit" name="submite-newslatter" value="Send">
-                                    </form>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- Footer widget -->
-                
-                <!-- Footer -->
-                <div class="footer-wrapper">
-                	<div class="container">
-                    	<div class="row">
-                        	<div class="span12">
-                            
-                            	<footer>
-                                	<div class="footer-nav">
-                                    	<ul>
-                                    		<li><a href="#">About US</a></li>
-                                    		<li><a href="#">News</a></li>
-                                    		<li><a href="#">Serbice</a></li>
-                                    		<li><a href="#">Recruiment </a></li>
-                                    		<li><a href="#">Media</a></li>
-                                    		<li>Chengdu Bus Company <a href="http://www.chengdubuscompany.com/" target="_blank" title="CBC">CBC</a> - Collect from <a href="http://www.chengdubuscompany.com/" title="CBC" target="_blank">CBC</a></li>
-                                    	</ul>
-                                    </div>
-                                    <a href="javascript:void(0)" onClick="goToByScroll('top')" class="gotop"></a>
-                                </footer>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Footer -->
-				
-                <div class="login-popup-wrapper">
-                    <div id="login-popup">
-                    	<h2>login Panel</h2>
-                        <form method="get" action="#">
-                            <input type="text" value="" id="username" placeholder="kavinhieu@gmail.com" />
-                            <input type="text" value="" id="password" placeholder="Password" />
-                            
-                            <input type="submit" value="sıgn ın" id="login-button"/>
-                        </form>
-                        <a href="#" class="close">Close</a>
-                    </div>
-                </div>
-
-                <!-- Scripts -->
+            <!-- Scripts -->
 				<script src="js/jquery-1.7.1.min.js"></script>
 				<script src="js/jquery.flexslider.js"></script>
                 <script src="js/jquery.flexslider-min.js"></script>
@@ -374,6 +305,7 @@ mysqli_close($connection);
                     });
                 </script>
                 <script src="js/custom.js"></script>		
-		</body>
+    
+    </body>
 </html>
-=======
+
