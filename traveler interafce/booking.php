a061f8df8baac882dba02d0c8444349c8fba7030
ZoeyYuezy <2390748081@qq.com>
2020-05-14 15:52:49 +0800

    update

--- traveler interafce/booking.php
+++ traveler interafce/booking.php
@@ -375,15 +375,15 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Ensure your information right?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Select "Commit" below if you ensure to buy this ticket.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="confirm.php">Logout</a>
+          <a class="btn btn-primary" href="confirm.php">Commit</a>
         </div>
       </div>
     </div>
